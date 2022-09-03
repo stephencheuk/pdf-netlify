70d804eedfcac57d6e3d9599e8de5172cffd9dea
stephencheuk <cheuklt@gmail.com>
2022-09-03 20:42:39 +0800

    add puppeteer-core

--- functions/meta.js
+++ functions/meta.js
@@ -1,4 +1,3 @@
-const chromium = require('chrome-aws-lambda')
 const bot = require('../bot')
 
 function generateTimeStampFileName() {
@@ -8,7 +7,7 @@ function generateTimeStampFileName() {
 
 exports.handler = async function (event, context) {
 
-  const pdfFileName = generateTimeStampFileName();
+  // const pdfFileName = generateTimeStampFileName();
 
   // const pdf = await bot({ url: "https://www.google.com" }, { width: 780, height: 1115 });
   const pdf = await bot({ file: "./pdf.htm" }, { width: 780, height: 1115 });
